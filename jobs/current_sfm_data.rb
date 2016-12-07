b29c12d79c326470f49beb79cec21dcb85ef6004
Peter Georgiev <georgiev2592@gmail.com>
2016-12-06 18:05:13 -0800

    updated current sfm data

--- jobs/current_sfm_data.rb
+++ jobs/current_sfm_data.rb
@@ -4,44 +4,34 @@ current_temperature_pr = 0
 current_humidity_pr = 0
 
 SCHEDULER.every '3s', :first_in => 0 do |job|
-  last_temperature_cp = current_temperature_cp
-  last_humidity_cp = current_humidity_cp
-  last_temperature_pr = current_temperature_pr
-  last_humidity_pr = current_humidity_pr
+  current_temperature_cp, current_humidity_cp = update_current_vals('cp', "Data", current_temperature_cp, current_humidity_cp)
+  current_temperature_pr, current_humidity_pr = update_current_vals('pr', "PeterRoom", current_temperature_pr, current_humidity_pr)
+end
 
+def update_current_vals(prefix, table_name, current_temperature, current_humidity)
+  last_temperature = current_temperature
+  last_humidity = current_humidity
 
   # mysql connection
   db = Mysql2::Client.new(host: "localhost", username: "sfmuser", password: "password", database: "sfm")
 
   # mysql query
-  sql = "SELECT * FROM Data ORDER BY created_at DESC LIMIT 1"
+  sql = "SELECT * FROM " + table_name + " ORDER BY created_at DESC LIMIT 1"
 
   # execute the query
   results = db.query(sql)
   
   # sending to List widget, so map to :label and :value
   results.map do |row|
-     current_temperature_cp = row['temp_f']
-     current_humidity_cp = row['humidity']
+     current_temperature = row['temp_f']
+     current_humidity = row['humidity']
   end
-  
-  # mysql query
-  sql = "SELECT * FROM PeterRoom ORDER BY created_at DESC LIMIT 1"
 
-  # execute the query
-  results = db.query(sql)
-  
-  # sending to List widget, so map to :label and :value
-  results.map do |row|
-     current_temperature_pr = row['temp_c']
-     current_humidity_pr = row['humidity']
-  end
-  
   db.close()
-  
-  send_event('cp_current_temperature', {current: current_temperature_cp, last: last_temperature_cp})
-  send_event('cp_current_humidity', {value: current_humidity_cp})
-  send_event('pr_current_temperature', {current: current_temperature_pr, last: last_temperature_pr})
-  send_event('pr_current_humidity', {value: current_humidity_pr})
+
+  send_event(prefix + '_current_temperature', {current: current_temperature, last: last_temperature})
+  send_event(prefix + '_current_humidity', {value: current_humidity})
+
+  return current_temperature, current_humidity
 end
 
